eca2d95fd92e95aef69a0bace07bb802837ffee5
Moises Saavedra <mmsaavedra1@uc.cl>
2020-09-04 16:50:04 -0400

    LASSO basico y avanzado

--- 4_Metodos-primer-orden/LASSO/LASSO_avanzado.py
+++ 4_Metodos-primer-orden/LASSO/LASSO_avanzado.py
@@ -4,6 +4,7 @@ __email__ = "mmsaavedra1@ing.puc.cl"
 # Se importan los modulos de python
 import numpy as np
 import scipy.linalg
+import time 
 
 # Se importan los modulos creados por el usuario
 from parametros import *
@@ -24,10 +25,11 @@ def timer(funcion):
         return resultado
     return inner
 
+@timer
 def LASSO(A, b, tau, iteracion_maxima):
     """
     Esta funcion recibe como input una matriz A y un vector b. Se busca resolver
-    el problema de regularizacion L1 con un parametro tau y haciendo un numero maximo
+    el problema de regulacion L1 con un parametro tau y haciendo un numero maximo
     de iteraciones definidas por el usuario, mediante el metodo del subgradiente.
 
     Su entrada posee:
@@ -39,6 +41,7 @@ def LASSO(A, b, tau, iteracion_maxima):
 
     Su salida es:
         - valor_optimo : Valor optimo del problema a minimizar.
+        - xk : Vector solucion del problema de k iteraciones.
 
     """
 
@@ -52,7 +55,14 @@ def LASSO(A, b, tau, iteracion_maxima):
 
     # Se setea el angulo entre las soluciones
     angulo = 0
-    a = 0.001
+
+    # Estimacion del valor de R (radio) y L (connstante de Lipschitz)
+    # segun teoria vista en clases
+    R = np.sqrt(np.linalg.norm(np.linalg.inv(np.dot(A, np.transpose(A))))*np.linalg.norm(b))
+    L = tau*np.sqrt(n) + np.linalg.norm(np.dot(A, np.transpose(A)))*R + np.linalg.norm(np.dot(np.transpose(A), b))
+
+    # Numero de iteraciones segun el teorema de la convergencia (NOS AHORAMOS EL LINESEARCH)
+    thetak = R/(L*np.sqrt(iteracion_maxima+1))
 
     # Se despliega el mensaje en pantalla
     print("\n\n**********    METODO DE SUBGRADIENTE    *********\n")
@@ -61,7 +71,7 @@ def LASSO(A, b, tau, iteracion_maxima):
     # Comienza el algoritmo
     for iteracion in range(iteracion_maxima):
 
-        # 1º Se calcula el subgradiente de la funcion objetivo
+        # 1º Se calcula el gradiente de la funcion objetivo
         subgradiente = 2*np.dot(np.transpose(A), np.dot(A, xk) - b) + tau*np.sign(xk)
 
         # 2º Se actualiza el pasado
@@ -69,33 +79,45 @@ def LASSO(A, b, tau, iteracion_maxima):
         xk_1 = xk
 
         # 3º Se actualiza el valor objetivo
-        theta = a/np.sqrt(iteracion+1)
-        xk = xk - theta*subgradiente
+        xk = xk - thetak*subgradiente
 
         # 4º Se evalua el error de ajuste
-        error = (np.linalg.norm(np.dot(A, xk) - b)) / np.linalg.norm(b)
+        error = np.amax(np.abs(np.dot(A, xk) - b)) / np.amax(np.abs(b))
 
         # 5º Se evalua el valor en la funcion objetivo
         valor = np.linalg.norm(np.dot(A, xk) - b, 2)**2 + tau*np.linalg.norm(xk, 1)
 
-        n1 = np.linalg.norm(xk, 1)
-        
+        if iteracion >= 3:
+            # 6º Se calcula el angulo entre las soluciones
+            diferencia1 = xk - xk_1
+            diferencia2 = xk_2 - xk_1
+            angulo = np.dot(np.transpose(diferencia2), diferencia1)/(np.linalg.norm(diferencia2)*np.linalg.norm(diferencia1))
+            angulo = angulo[0][0]
+
+
          # La rutina de subgradiente muestra en pantalla para cada iteracion:
         # nº de iteracion, valor de la funcion evaluada en el x de la iteracion,
-        #  error de ajuste y la norma 1 de xk.
-        retorno_en_pantalla = [iteracion, valor, error, n1]
-#        print(f"{retorno_en_pantalla[0]: ^12d}{retorno_en_pantalla[1]: ^12f} {retorno_en_pantalla[2]: ^12f} {retorno_en_pantalla[3]: ^12f}")
-        print("%12.6f %12.6f %12.6f %12.6f" % (retorno_en_pantalla[0],retorno_en_pantalla[1],retorno_en_pantalla[2],retorno_en_pantalla[3]))
-    return xk
+        #  error y angulo formado por las soluciones.
+        retorno_en_pantalla = [iteracion, valor, error, angulo]
+        print(f"{retorno_en_pantalla[0]: ^12d}{retorno_en_pantalla[1]: ^12f} {retorno_en_pantalla[2]: ^12f} {retorno_en_pantalla[3]: ^12f}")
+
 
 if __name__ == '__main__':
     # Esto es para que simepre se generen los mismos numeros aleatorios
     np.random.seed(1000)
 
-    tau = 0.5
-    iteracion_maxima = 20000
-    
-    A, b = generar_datos(50, 300)
-    xsol = LASSO(A, b, tau, iteracion_maxima)
+    # Esto es aprueba de errores de singularidad
+    while True:
+        try:
+            # Setea las dimensiones deseadas
+            A, b = generar_datos(30, 100)
+            # Se prueba la singularidad (NO HACER CASO A ESTA LINEA)
+            prueba = np.linalg.inv(A)
 
+        except:
+            print("¡Error de singularidad!")
 
+        finally:
+            # Se corre el programa
+            LASSO(A, b, 10, 20000)
+            break
